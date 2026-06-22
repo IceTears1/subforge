2423c31e22105f1a816475617549d5fa6b684d22
ice <642203775@qq.com>
2026-06-22 11:06:43 +0800

    release: v1.2.2 修复 base64 订阅格式，优化订阅输出

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SubForge One-Click Installer v1.2.1
+# SubForge One-Click Installer v1.2.2
 # Usage: curl -fsSL https://raw.githubusercontent.com/IceTears1/subforge/main/install.sh | sudo bash
 #    or: sudo bash install.sh
 
@@ -20,7 +20,7 @@ NC='\033[0m'
 
 REPO="https://github.com/IceTears1/subforge.git"
 INSTALL_DIR="/opt/subforge"
-VERSION="1.2.1"
+VERSION="1.2.2"
 
 # Default ports (configurable via interactive prompts)
 FRONTEND_PORT=3001
